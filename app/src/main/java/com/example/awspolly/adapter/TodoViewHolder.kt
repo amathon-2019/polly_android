ab842913fcad6f56c3886921832bd31c138ddd0b
tinyjin <baram991103@gmail.com>
2019-09-01 04:00:37 +0900

    리스트 아이템 UI 수정

--- app/src/main/java/com/example/awspolly/adapter/TodoViewHolder.kt
+++ app/src/main/java/com/example/awspolly/adapter/TodoViewHolder.kt
@@ -10,10 +10,8 @@ import kotlinx.android.synthetic.main.todo_view_item.view.*
 class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var todoText: TextView? = itemView.todo_text
-    var todoDate: ViewGroup? = itemView.todo_date
 
     fun bind(item: TodoListItem) {
         todoText?.text = item.content
-        todoDate?.visibility = if (item.isShowDate == 1) View.VISIBLE else View.INVISIBLE
     }
 }
