376f983636347f21cc6540c66aa3ead3be9904c3
tinyjin <baram991103@gmail.com>
2019-08-31 23:31:05 +0900

    메인 UI 디자인

--- app/src/main/java/com/example/awspolly/adapter/TodoViewHolder.kt
+++ app/src/main/java/com/example/awspolly/adapter/TodoViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.awspolly.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.awspolly.data.TodoListItem
@@ -9,8 +10,10 @@ import kotlinx.android.synthetic.main.todo_view_item.view.*
 class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     var todoText: TextView? = itemView.todo_text
+    var todoDate: ViewGroup? = itemView.todo_date
 
     fun bind(item: TodoListItem) {
         todoText?.text = item.content
+        todoDate?.visibility = if (item.isShowDate == 1) View.VISIBLE else View.INVISIBLE
     }
 }
--- app/src/main/java/com/example/awspolly/data/TodoListItem.kt
+++ app/src/main/java/com/example/awspolly/data/TodoListItem.kt
@@ -2,5 +2,6 @@ package com.example.awspolly.data
 
 data class TodoListItem(
     var content: String,
-    var todoDate: String
+    var todoDate: String,
+    var isShowDate: Int
 )
